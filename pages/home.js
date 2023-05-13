45d4038a4905f316989e4503b9c51f2e00f8cdc0
guidodika <guidodikafirguston.mail@gmail.com>
2023-05-14 00:36:10 +0700

    fix size text

--- pages/home.js
+++ pages/home.js
@@ -24,10 +24,10 @@ function Home() {
               <AnimatedText texts={dynamicTexts} />
               <h2 className="pl-2">Semuanya</h2>
             </div>
-            <h1 className="text-2xl font-extrabold md:text-5xl py-1 ">
+            <h1 className="text-[28px] font-extrabold md:text-5xl py-1 ">
               Guido Dika Firguston
             </h1>
-            <h2 className="text-xl py-2 ">Seorang Frontend Developer</h2>
+            <h2 className="text-lg py-2 ">Seorang Frontend Developer</h2>
 
             <Link href="/about">
               <button className="p-3 bg-gray-600 hover:bg-gray-500 text-white rounded-3xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
